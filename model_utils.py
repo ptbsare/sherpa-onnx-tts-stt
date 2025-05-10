1986c12bd6ff908e08cf992cb0a5ccc0f3ac16bb
ptbsare <496725701@qq.com>
2025-05-11 00:47:29 +0800

    chore(version): bump version v0.3.4

--- model_utils.py
+++ model_utils.py
@@ -69,8 +69,7 @@ def initialize_models(cli_args):
                     sample_rate=16000,
                     feature_dim=80,
                     debug=cli_args.debug,
-                    rule_fsts=os.path.join('/app/',
-                                            "itn_zh_number.fst" if cli_args.stt_builtin_auto_convert_number == True else ""),
+                    rule_fsts=(os.path.join('/app/', "itn_zh_number.fst") if cli_args.stt_builtin_auto_convert_number else ''),
                 )
             except Exception as e:  # More specific exception handling is better
                 _LOGGER.exception("Failed to initialize STT model:")
@@ -87,8 +86,7 @@ def initialize_models(cli_args):
                 sample_rate=16000,
                 feature_dim=80,
                 debug=cli_args.debug,
-                rule_fsts=os.path.join('/app/',
-                                        "itn_zh_number.fst" if cli_args.stt_builtin_auto_convert_number == True else ""),
+                rule_fsts=(os.path.join('/app/', "itn_zh_number.fst") if cli_args.stt_builtin_auto_convert_number else ""),
             )
             except Exception as e:  # More specific exception handling is better
                 _LOGGER.exception("Failed to initialize STT model:")
